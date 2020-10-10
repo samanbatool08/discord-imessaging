54b5dd20faf8bf3187fd4043c1260bdb36268dee
Your Name <samanbatool08@gmail.com>
2020-10-10 16:28:16 -0400

    voice styling

--- discord-app/src/Sidebar.js
+++ discord-app/src/Sidebar.js
@@ -4,6 +4,8 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
 import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
 
 function Sidebar() {
     return (
@@ -39,6 +41,10 @@ function Sidebar() {
             <div className='sidebar__voiceInfo'>
                 <h3>Voice Connected</h3>
                 <p>Stream</p>
+            </div>
+            <div className='sidebar__voiceIcons'>
+                <InfoOutlinedIcon />
+                <CallIcon />
             </div>
         </div>
     </div>

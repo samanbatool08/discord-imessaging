155ef37d1ac9b2afae0312ac93b7a56bf802e11b
Your Name <samanbatool08@gmail.com>
2020-10-10 14:43:44 -0400

    sidebar component

--- discord-app/src/App.js
+++ discord-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
       <h1>Let's build discord!! </h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }

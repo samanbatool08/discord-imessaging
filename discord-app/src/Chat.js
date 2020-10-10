124157668716c1df6feb3866842fe8662074bdf7
Your Name <samanbatool08@gmail.com>
2020-10-10 17:35:36 -0400

    set up chat header

--- discord-app/src/Chat.js
+++ discord-app/src/Chat.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Chat.css';
+import ChatHeader from './ChatHeader';
 
 function Chat() {
     return (
         <div className='chat'> 
             <h2>I am the Chat Screen</h2>
+            <ChatHeader />
         </div>
     )
 }
---
+++ discord-app/src/ChatHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatHeader.css';
+
+function ChatHeader() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatHeader;

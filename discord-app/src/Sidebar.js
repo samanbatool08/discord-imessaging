3672bb451d293e87d6867cb3a760d2c4228ba3f1
Your Name <samanbatool08@gmail.com>
2020-10-10 16:03:27 -0400

    sidebar styling

--- discord-app/src/Sidebar.js
+++ discord-app/src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -10,9 +11,21 @@ function Sidebar() {
             <div className='sidebar__top'>
                 <h3>Dinner Plans</h3>
                 <ExpandMoreIcon />
-
             </div>
-            
+
+            <div className='sidebar__channels'>
+                <div className='sidebar__channelsHeader'>
+                    <div className='sidebar__header'>
+                        <ExpandMoreIcon /> 
+                        <h4>Text Channels</h4>
+                    </div>
+                    <AddIcon className='sidebar__addChannel' />
+                </div>
+            </div>
+
+            <div className='sidebar__channelsList'>
+                <SidebarChannel />
+            </div>
         </div>
     )
 }

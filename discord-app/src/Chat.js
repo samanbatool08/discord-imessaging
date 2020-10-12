0e225a1e513f174f90207bca91da36b98408e105
Your Name <samanbatool08@gmail.com>
2020-10-11 21:55:15 -0400

    message form

--- discord-app/src/Chat.js
+++ discord-app/src/Chat.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Chat.css';
 import ChatHeader from './ChatHeader';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function Chat() {
     return (
         <div className='chat'> 
             <ChatHeader />
+
+            <div className='chat__messages'>
+
+            </div>
+
+            <div className='chat__input'>
+                <AddCircleIcon fontSize='large'/>
+                <form>
+                    <input placeholder={`message #TESTCHANNEL`} />
+                </form>
+            </div>
         </div>
+
+
     )
 }
 

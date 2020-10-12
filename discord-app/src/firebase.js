213565740fbd4faf1a6891e11e9647f53631760e
Your Name <samanbatool08@gmail.com>
2020-10-12 10:48:25 -0400

    setup auth, db

--- discord-app/src/firebase.js
+++ discord-app/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyC1F_0jxkpXIR0MjSDHgd1ycB0WYfeqoU8",
     authDomain: "discord-im.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:1052062594683:web:95759fcc3df968e0f254de",
     measurementId: "G-Q8MEXNWS0D"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth(); 
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider }; 
+  export default db; 

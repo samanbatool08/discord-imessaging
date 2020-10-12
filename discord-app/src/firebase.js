bdfcf4322cb75a7410d27db40ff6ccb27e576984
Your Name <samanbatool08@gmail.com>
2020-10-12 10:45:35 -0400

    firebase config

---
+++ discord-app/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1F_0jxkpXIR0MjSDHgd1ycB0WYfeqoU8",
+    authDomain: "discord-im.firebaseapp.com",
+    databaseURL: "https://discord-im.firebaseio.com",
+    projectId: "discord-im",
+    storageBucket: "discord-im.appspot.com",
+    messagingSenderId: "1052062594683",
+    appId: "1:1052062594683:web:95759fcc3df968e0f254de",
+    measurementId: "G-Q8MEXNWS0D"
+  };

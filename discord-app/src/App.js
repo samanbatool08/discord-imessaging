13a92db962da31fd9d751840def328b6a3db56ba
Your Name <samanbatool08@gmail.com>
2020-10-12 12:15:21 -0400

    user persisting

--- discord-app/src/App.js
+++ discord-app/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Login from './Login';
 import { selectUser } from './features/userSlice';
+import { auth } from 'firebase';
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth().onAuthStateChanged((authUser) => {
+      console.log('user is', authUser)
+      if (authUser) {
+        // user is logged in
+      } else {
+        // user is logged out
+      }
+    })
+  }, [])
 
   return (
     //BEM naming convention

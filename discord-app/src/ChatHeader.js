04a570c357ecbff64f9a78ccd96de0400fb54dbe
Your Name <samanbatool08@gmail.com>
2020-10-11 21:24:26 -0400

    chat header setup with material ui icons

--- discord-app/src/ChatHeader.js
+++ discord-app/src/ChatHeader.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './ChatHeader.css';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+
 
 function ChatHeader() {
     return (
         <div>
-            
+            <h3>I am the chat header</h3>
+            <div className='chatHeader__left'>
+                <h3>
+                    <span className='chatHeader__hash'>
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+
+            <div className='chatHeader__right'>
+                <NotificationsIcon />
+                <EditLocationRoundedIcon />
+                <PeopleAltRoundedIcon />
+
+            </div>
         </div>
     )
 }

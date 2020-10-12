ebc08a77979faa76b2762f60db3896cbb762cff2
Your Name <samanbatool08@gmail.com>
2020-10-11 22:01:31 -0400

    input icons

--- discord-app/src/Chat.js
+++ discord-app/src/Chat.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Chat.css';
 import ChatHeader from './ChatHeader';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 function Chat() {
     return (
@@ -16,7 +19,16 @@ function Chat() {
                 <AddCircleIcon fontSize='large'/>
                 <form>
                     <input placeholder={`message #TESTCHANNEL`} />
+                    <button 
+                    className='chat__inputButton'
+                    type='submit'>Send Message</button>
                 </form>
+
+                <div className='chat__inputIcons'>
+                    <CardGiftcardIcon fontSize='large'/>
+                    <GifIcon fontSize='large'/>
+                    <EmojiEmotionsIcon fontSize='large'/>
+                </div>
             </div>
         </div>
 

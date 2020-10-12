ffedd00d87ab8614210e707def1bd43cd5b6e0e7
Your Name <samanbatool08@gmail.com>
2020-10-12 11:50:39 -0400

    set up login reducer

--- discord-app/src/App.js
+++ discord-app/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     //BEM naming convention
     <div className="app">
-
-      <Sidebar />
-
-      {/* Chat */}
-      <Chat />
+      {user ? ( 
+        <>
+        <Sidebar />
+        <Chat />
+        </>
+      ) : (
+        <h2>You need to log in </h2>
+      )}
     </div>
   );
 }

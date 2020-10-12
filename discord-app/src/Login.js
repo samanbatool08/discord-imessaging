6caa77092d7bb4dee75c20e3d1e45d048f41edd5
Your Name <samanbatool08@gmail.com>
2020-10-12 12:01:52 -0400

    login styling

--- discord-app/src/Login.js
+++ discord-app/src/Login.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Login.css';
+import {Button} from '@material-ui/core';
 
 function Login() {
+    const signIn = () => {
+        //login logic
+    }
     return (
         <div className='login'>
-            <h2>I am the login page</h2>
+            <div className='login__logo'>
+                <img src='https://www.adl.org/sites/default/files/styles/open_graph_image_1200_x_628_/public/2019-08/discord-logo.jpg?itok=LMNTgq_N'
+                alt = '' />
+            </div>
+
+            <Button onClick={signIn}>Sign In</Button>
         </div>
     )
 }
